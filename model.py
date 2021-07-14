ab4537733bb867efc556917ab8eae87cffb76581
task4233 <29667656+task4233@users.noreply.github.com>
2021-07-14 17:58:02 +0900

    refactor: 必要のないコードを削除a

--- model.py
+++ model.py
@@ -3,8 +3,9 @@ import numpy as np
 from keras.models import model_from_json
 import pickle
 
-f = open("list_row.txt","rb")
-list_row = pickle.load(f)
+list_row = None
+with open("list_row.txt", "rb") as f:
+  list_row = pickle.load(f)
 
 with open("model_400_epochs.model", 'r') as f:
     json_string = f.read()
@@ -12,11 +13,11 @@ model = model_from_json(json_string)
 
 model.load_weights('param_400_epochs.hdf5')
 
+
 def predict_font(img):
-  
-  im_resized = np.resize(img,(64,64,3)).reshape(1,64,64,3).astype("f")
+  im_resized = np.resize(img, (64, 64, 3)).reshape(1, 64, 64, 3).astype("f")
   im_resized /= 255
-  
+
   pred = model.predict(im_resized)
-  
+
   return list_row[np.argmax(pred)]
