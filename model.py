97be9e0bb62dd7929392bfa70a79a1a7f3b14a6a
Kodai Yokoyama <aaaaa333445@gmail.com>
2021-07-26 21:03:54 +0900

    revised

--- model.py
+++ model.py
@@ -3,10 +3,11 @@ import numpy as np
 from keras.models import model_from_json
 import pickle, sys
 
-with open("list_row.txt", "rb") as f:
+with open("list_row.pkl", "rb") as f:
   list_row = pickle.load(f)
 
-with open("model_.model", 'r') as f:
+
+with open("model.model", 'r') as f:
     json_string = f.read()
 model = model_from_json(json_string)
 
@@ -15,12 +16,11 @@ model.load_weights('param.hdf5')
 
 def predict_font(img):
   im_resized = np.resize(img, (64, 64, 3)).reshape(1, 64, 64, 3).astype("f")
-  im_resized /= 255
-
   pred = model.predict(im_resized)
-
+  
   try:
     font_name = list_row[np.argmax(pred)]
+    print(np.argmax(pred))
     idx = font_name.find(".")
     r = font_name[:idx]
   except Exception as e:
