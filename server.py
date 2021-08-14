359855eada64e61062d3436e58da22b8f3455da6
Yamaguchi-shuhei <81962317+Yamaguchi-shuhei@users.noreply.github.com>
2021-08-14 20:57:35 +0900

    16×16のアイコンの追加(うまくいっていない)

--- server.py
+++ server.py
@@ -9,6 +9,14 @@ import sys
 import uuid
 import traceback
 import model
+from flask import send_from_directory
+
+@app.route('/favicon.ico')
+def favicon():
+
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                          'favicon.ico',mimetype='image/vnd.microsoft.icon')
+
 
 app = Flask(__name__, static_url_path="/static")
 
