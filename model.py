11a3fbdeacf409b6c735635f815113100b04feaf
Kodai Yokoyama <aaaaa333445@gmail.com>
2021-08-31 17:24:43 +0900

    errorが解決できない

--- model.py
+++ model.py
@@ -2,6 +2,7 @@ from PIL import Image
 import numpy as np
 from keras.models import model_from_json
 import pickle, sys, traceback
+from tensorflow.keras import models
 
 class FontData:
   def __init__(self, name, probability) -> None:
@@ -15,19 +16,24 @@ with open("list_row.pkl", "rb") as f:
   for idx, name in enumerate(list_row):
     list_row[idx] = list_row[idx][:list_row[idx].find(".")]
 
-with open("model.model", 'r') as f:
+with open("model/alex_net/model.model", 'r') as f:
     json_string = f.read()
 model = model_from_json(json_string)
 
-model.load_weights('param.hdf5')
+model.load_weights('model/alex_net/param.hdf5')
+
+model_google = models.load_model('model/google_net')
+model_google.load_weights('model/google_net')
 
 def predict_font(img, display_num : int = 1):
   IMG_SIZE = (64, 64, 3)
   pil_image = Image.fromarray(img).convert("L")
   img = np.array(pil_image)
   im_resized = np.resize(img, IMG_SIZE).reshape(1, 64, 64, 3).astype("f")
-  pred = model.predict(im_resized)
-  
+  pred_1= model.predict(im_resized)
+  im_resized_norm = im_resized /255
+  pred_2 = model_google.predict(im_resized_norm)
+  pred = (pred_1 + pred_2)/2
   try:
     # 降順にソートして、前からdisplay_num分取得する
     idxs = np.argsort(-pred[0])[:display_num]
