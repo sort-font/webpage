4d391cd53be5d6f199e8dc24873b81b11f8a3492
task4233 <29667656+task4233@users.noreply.github.com>
2021-09-19 01:20:32 +0900

    feat: run autopep8

--- test_server.py
+++ test_server.py
@@ -2,11 +2,13 @@ import pytest
 
 from server import app
 
+
 @pytest.fixture
 def client():
     app.config['TESTING'] = True
     client = app.test_client()
     yield client
+
 
 def test_top(client):
     """ test top page """
